1a4001e87b3ef330b8ba67e8f37501f52963f980
Marco Monelli <marcomonelli3@gmail.com>
2025-10-07 17:35:25 +0200

    Create 01_my_first_R_script.R

---
+++ Code/01_my_first_R_script.R
@@ -0,0 +1,15 @@
+# This is a comment and will help humans understand 
+
+2 + 3
+
+naza <- 2 + 3
+
+marco <- 7 + 4
+
+vale <- c(5, 10, 13, 20, 30)
+
+elia <- c(2, 5, 10, 40, 70)
+
+plot(elia, vale)
+
+#If I want to change the point symbol I will use pch
