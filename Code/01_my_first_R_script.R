df80862474ace0a003d19dc51ad40a66cb88aade
Marco Monelli <marcomonelli3@gmail.com>
2025-10-29 10:51:12 +0100

    Update 01_my_first_R_script.R

--- Code/01_my_first_R_script.R
+++ Code/01_my_first_R_script.R
@@ -6,10 +6,14 @@ naza <- 2 + 3
 
 marco <- 7 + 4
 
-vale <- c(5, 10, 13, 20, 30)
+vale <- c(5, 10, 13, 20, 30) #samples of insect species richness
 
-elia <- c(2, 5, 10, 40, 70)
+elia <- c(2, 5, 10, 40, 70) #species numbers of different taxa
 
-plot(elia, vale)
+plot(elia, vale) #graph about the two parameters
 
 #If I want to change the point symbol I will use pch
+
+plot(elia, vale, xlab="all taxa species richness", ylab="insects species richness", col="blue", pch=19, cex=.5)
+
+#graph with names of axis (xlab, ylab), colored points (col), type of points (pch), size of points (cex)
